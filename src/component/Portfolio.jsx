813ed279e12bdcae4fb61e3c26621b30bf03b20f
mdshahadatalamm <mdshahadatalam25@gmail.com>
2024-12-15 10:31:12 -0800

    link

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import port from '../assets/images/portfolio/port.png'
 import { IoIosArrowRoundForward } from "react-icons/io";
+import projectImg from '../assets/images/projectImg/Aytor.png'
+import projectImg2 from '../assets/images/projectImg/node.png'
 
 export const Portfolio = () => {
   return (
@@ -28,78 +30,36 @@ export const Portfolio = () => {
                 </div>
                 <div className="col-lg-4">
                 <div className='portfolioCard MyImg mt-4'>
-                    <div className='d-flex justify-center'><img className='img-fluid ' src={port} alt="image" /></div>
-                    <div className='p-3'>
-                    <small className='uiUX'>UI-UX DESIGN</small>
-                    <h4 className='product'>Product Admin Dashboard</h4>
+                    <div className='d-flex justify-center'><img className='img-fluid ' src={projectImg} alt="image" /></div>
+                    <div className='p-4'>
+                    <small className='uiUX'>Frontend DESIGN</small>
+                    <h4 className='product'>Aytor e-commerce website</h4>
                     <p className='portDami'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
-
-                     <span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
+                     <a target='none' className='cv ' href="https://aytorw-commerce.netlify.app/"><span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
                      <span className='text-2xl pt-1 ps-1'><IoIosArrowRoundForward /></span>
-                     </span>
+                     </span></a>
+                  
+                    
                     </div>
                   </div>
                 </div>
                 <div className="col-lg-4">
                 <div className='portfolioCard MyImg mt-4'>
-                    <div className='d-flex justify-center'><img className='img-fluid' src={port} alt="image" /></div>
+                    <div className='d-flex justify-center'><img className='img-fluid' src={projectImg2} alt="image" /></div>
                     <div className='p-3'>
                     <small className='uiUX'>UI-UX DESIGN</small>
                     <h4 className='product'>Product Admin Dashboard</h4>
                     <p className='portDami'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
 
-                     <span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
+                     <a target='none' className='cv' href="https://note-book-umber.vercel.app/"><span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
                      <span className='text-2xl pt-1 ps-1'><IoIosArrowRoundForward /></span>
-                     </span>
+                     </span></a>
                     </div>
                   </div>
                 </div>
             </div>
            
-            <div className="row font-serif">
-                <div className="col-lg-4">
-                  <div className='portfolioCard MyImg mt-4'>
-                    <div className='d-flex justify-center'><img className='img-fluid ' src={port} alt="image" /></div>
-                    <div className='p-3'>
-                    <small className='uiUX'>UI-UX DESIGN</small>
-                    <h4 className='product'>Product Admin Dashboard</h4>
-                    <p className='portDami'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
-
-                     <span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
-                     <span className='text-2xl pt-1 ps-1'><IoIosArrowRoundForward /></span>
-                     </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                <div className='portfolioCard MyImg mt-4'>
-                    <div className='d-flex justify-center'><img className='img-fluid ' src={port} alt="image" /></div>
-                    <div className='p-3'>
-                    <small className='uiUX'>UI-UX DESIGN</small>
-                    <h4 className='product'>Product Admin Dashboard</h4>
-                    <p className='portDami'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
-
-                     <span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
-                     <span className='text-2xl pt-1 ps-1'><IoIosArrowRoundForward /></span>
-                     </span>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-lg-4">
-                <div className='portfolioCard MyImg mt-4'>
-                    <div className='d-flex justify-center'><img className='img-fluid' src={port} alt="image" /></div>
-                    <div className='p-3'>
-                    <small className='uiUX'>UI-UX DESIGN</small>
-                    <h4 className='product'>Product Admin Dashboard</h4>
-                    <p className='portDami'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
-
-                     <span className='d-flex align-items-center case transition duration-300 ease-in-out transform hover:bg-[#8A2BFF] hover:scale-105 hover:shadow-lg hover:text-white'> Case Study 
-                     <span className='text-2xl pt-1 ps-1'><IoIosArrowRoundForward /></span>
-                     </span>
-                    </div>
-                  </div>
-                </div>
-            </div>
+            
             
 
             <a className='cv' target='none' href="https://github.com/mdshahadatalam"> <div className='d-flex justify-center mt-5'>

a9df4b61be94b5e6de26f62545bb1915ea799f8a
mdshahadatalamm <mdshahadatalam25@gmail.com>
2024-12-26 22:36:49 -0800

    test2

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -37,8 +37,10 @@ export const Contact = () => {
  }
 
  const handleSubmit =()=>{
+  setLoader(true)
   console.log(name,email,location,subject,message)
   axios.post('https://my-portfolio-backend-wheat.vercel.app/sendEmail',{name,email,location,subject,message}).then(res=>{
+    setLoader(false)
     console.log(res);
     setName('')
     setEmail('')
@@ -58,6 +60,7 @@ export const Contact = () => {
       });
     
   }).catch(err=>{
+    setLoader(false)
     console.log(err);
     toast.error('Please try again', {
       position: "top-right",
@@ -249,9 +252,8 @@ export const Contact = () => {
                   <div className='mt-3'> 
                   <button  onClick={handleSubmit} className='submit transition duration-300 ease-in-out transform hover:bg-purple-500 hover:scale-105 hover:shadow-lg'>
                     {
-                      loader ? <SyncLoader  size={5} color='white'/> : "Submit" 
+                      loader ? <SyncLoader  size={5} color='white'/> : `Submit ${<VscSend />}`
                     }
-                    <VscSend />
                     
                     </button>
                   </div>
